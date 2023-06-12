45aa392bf45c815ee8ac74e650ae5405ca850d60
PAULO VICTOR CAETANO DE OLIVEIRA PRPS402796 <prps402796@pbh.rmi>
2023-06-12 16:22:16 -0300

    schema do controle de cadms ok

--- server/src/controle-cadms/controle-cadms.service.ts
+++ server/src/controle-cadms/controle-cadms.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
 import { CreateControleCadmDto } from './dto/create-controle-cadm.dto';
 import { UpdateControleCadmDto } from './dto/update-controle-cadm.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -10,13 +10,24 @@ export class ControleCadmsService {
   async create(data: Prisma.controle_cadmsCreateInput): Promise<string> {
     return this.prisma.controle_cadms.create({
       data,
-    }).then(() => {
-      return "sigesp cadastrado com sucesso!"
-    });
+    })
+      .then(() => {
+        return "sigesp cadastrado com sucesso!"
+      })
+      .catch((err) => {
+        return err.message
+      })
+    ;
   }
 
   findAll() {
     return this.prisma.controle_cadms.findMany()
+      .then((res) => {
+        if(res.length === 0){
+          return "Não há nenhum sigesp cadastrado"
+        }
+        return res
+      })
   }
 
   findOne(id: number) {
--- server/src/controle-cadms/dto/create-controle-cadm.dto.ts
+++ server/src/controle-cadms/dto/create-controle-cadm.dto.ts
@@ -1,8 +1,3 @@
 export class CreateControleCadmDto {
   sigesp: string
-  status: string
-  cargo: string
-  especialidade: string
-  carga_horaria: number
-  unidade: string
 }
